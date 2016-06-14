ce07b7219501436be05e965b9c6c638c7775a659
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-06-14 17:30:09 -0400

    refactored views using helpers and added navigation

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,5 +3,29 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
-  
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_url
+      # http://localhost:3000/pictures
+      # /pictures
+    else
+      render :new
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 end
